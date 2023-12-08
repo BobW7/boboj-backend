b4fc7f232c3a8691111fa9d44ea7fab2ec419b1a
BobW7 <1186717311@qq.com>
2023-12-08 17:10:41 +0800

    Initial commit

---
+++ src/main/java/com/yupi/boboj/controller/PostThumbController.java
@@ -0,0 +1,56 @@
+package com.yupi.boboj.controller;
+
+import com.yupi.boboj.common.BaseResponse;
+import com.yupi.boboj.common.ErrorCode;
+import com.yupi.boboj.common.ResultUtils;
+import com.yupi.boboj.exception.BusinessException;
+import com.yupi.boboj.model.dto.postthumb.PostThumbAddRequest;
+import com.yupi.boboj.model.entity.User;
+import com.yupi.boboj.service.PostThumbService;
+import com.yupi.boboj.service.UserService;
+import javax.annotation.Resource;
+import javax.servlet.http.HttpServletRequest;
+import lombok.extern.slf4j.Slf4j;
+import org.springframework.web.bind.annotation.PostMapping;
+import org.springframework.web.bind.annotation.RequestBody;
+import org.springframework.web.bind.annotation.RequestMapping;
+import org.springframework.web.bind.annotation.RestController;
+
+/**
+ * 帖子点赞接口
+ *
+ * @author <a href="https://github.com/liyupi">程序员鱼皮</a>
+ * @from <a href="https://yupi.icu">编程导航知识星球</a>
+ */
+@RestController
+@RequestMapping("/post_thumb")
+@Slf4j
+public class PostThumbController {
+
+    @Resource
+    private PostThumbService postThumbService;
+
+    @Resource
+    private UserService userService;
+
+    /**
+     * 点赞 / 取消点赞
+     *
+     * @param postThumbAddRequest
+     * @param request
+     * @return resultNum 本次点赞变化数
+     */
+    @PostMapping("/")
+    public BaseResponse<Integer> doThumb(@RequestBody PostThumbAddRequest postThumbAddRequest,
+            HttpServletRequest request) {
+        if (postThumbAddRequest == null || postThumbAddRequest.getPostId() <= 0) {
+            throw new BusinessException(ErrorCode.PARAMS_ERROR);
+        }
+        // 登录才能点赞
+        final User loginUser = userService.getLoginUser(request);
+        long postId = postThumbAddRequest.getPostId();
+        int result = postThumbService.doPostThumb(postId, loginUser);
+        return ResultUtils.success(result);
+    }
+
+}
