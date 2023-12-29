079191517cf0931ff1398c5da4b5e8215494a8ec
BobW7 <1186717311@qq.com>
2023-12-29 15:26:22 +0800

    题目表设计

--- sql/create_table.sql
+++ sql/create_table.sql
@@ -44,17 +44,21 @@ create table if not exists post
 -- 题目表
 create table if not exists question
 (
-    id         bigint auto_increment comment 'id' primary key,
-    title      varchar(512)                       null comment '标题',
-    content    text                               null comment '内容',
-    tags       varchar(1024)                      null comment '标签列表（json 数组）',
-    answer     text                               null comment '题目答案',
-    submitNum  int      default 0                 not null comment '题目提交数',
-    acceptNum  int      default 0                 not null comment '题目通过数',
-    userId     bigint                             not null comment '创建用户 id',
-    createTime datetime default CURRENT_TIMESTAMP not null comment '创建时间',
-    updateTime datetime default CURRENT_TIMESTAMP not null on update CURRENT_TIMESTAMP comment '更新时间',
-    isDelete   tinyint  default 0                 not null comment '是否删除',
+    id          bigint auto_increment comment 'id' primary key,
+    title       varchar(512)                       null comment '标题',
+    content     text                               null comment '内容',
+    tags        varchar(1024)                      null comment '标签列表（json 数组）',
+    answer      text                               null comment '题目答案',
+    submitNum   int      default 0                 not null comment '题目提交数',
+    acceptNum   int      default 0                 not null comment '题目通过数',
+    judgeCase   text                               null comment '判题用例（JSON数组）',
+    judgeConfig text                               null comment '判题配置（JSON对象）',
+    thumbNum    int      default 0                 not null comment '点赞数',
+    favourNum   int      default 0                 not null comment '收藏数',
+    userId      bigint                             not null comment '创建用户 id',
+    createTime  datetime default CURRENT_TIMESTAMP not null comment '创建时间',
+    updateTime  datetime default CURRENT_TIMESTAMP not null on update CURRENT_TIMESTAMP comment '更新时间',
+    isDelete    tinyint  default 0                 not null comment '是否删除',
     index idx_userId (userId)
 ) comment '帖子' collate = utf8mb4_unicode_ci;
 
